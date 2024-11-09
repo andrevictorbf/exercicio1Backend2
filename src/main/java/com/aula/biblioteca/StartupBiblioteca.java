2143499c3522313da63f52dd77b454dd83530771
andrevictorbf <andrevictor_barcia@hotmail.com>
2024-11-09 15:11:42 -0300

    resolucao da questao 2

--- src/main/java/com/aula/biblioteca/StartupBiblioteca.java
+++ src/main/java/com/aula/biblioteca/StartupBiblioteca.java
@@ -1,26 +1,28 @@
 package com.aula.biblioteca;
 
-import com.aula.biblioteca.repository.LivroRepository;
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.aula.biblioteca.repository.LivroRepository;
+
 @SpringBootApplication
 public class StartupBiblioteca {
 
-	@Autowired
-	private LivroRepository livroRepository;
+    @Autowired
+    private LivroRepository livroRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(StartupBiblioteca.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StartupBiblioteca.class, args);
+    }
 
-
-	@PostConstruct
-	public void MongoDBTest(){
-		//System.out.println(livroRepository.findByTitulo("Livro B"));
-		//System.out.println(livroRepository.findByNotaAndQtdePaginasGreaterThanEqual(300, 8f));
-	}
+    // @Test
+    // void main() {
+    //     BibliotecaApplication.main(new String[]{});
+    // }
+	public void MongoDBTest() {
+        //System.out.println(livroRepository.findByTitulo("Livro B"));
+        //System.out.println(livroRepository.findByNotaAndQtdePaginasGreaterThanEqual(300, 8f));
+    }
 
 }
--- src/main/java/com/aula/biblioteca/controller/AutorController.java
+++ src/main/java/com/aula/biblioteca/controller/AutorController.java
@@ -1,7 +1,22 @@
 package com.aula.biblioteca.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.aula.biblioteca.dto.AutorDto;
 import com.aula.biblioteca.service.AutorService;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -9,15 +24,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.print.attribute.standard.Media;
 
 @RequiredArgsConstructor
 @RestController
@@ -34,12 +40,19 @@ public class AutorController {
     public ResponseEntity<Page<AutorDto>> findAll(@Parameter(required = true) @PageableDefault(size = 5) Pageable pageable){
         return ResponseEntity.ok(autorService.findAll(pageable));
     }
-
+    
+    @Operation(description = "Inserir novo autor no banco de dados.", summary = "Inserir autor.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Autor criado", content = @Content(mediaType = "application/json", schema=@Schema(implementation = Page.class)))
+    })
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AutorDto> save(@RequestBody AutorDto autorDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(autorService.save(autorDto));
     }
-
+    @Operation(description = "Alterar instancia usuario no banco de dados.", summary = "Alterar autor.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Autor alterado", content = @Content(mediaType = "application/json", schema=@Schema(implementation = Page.class)))
+    })
     @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AutorDto> update(@PathVariable String id, @RequestBody AutorDto autorDto){
         return ResponseEntity.ok(autorService.update(id, autorDto));
